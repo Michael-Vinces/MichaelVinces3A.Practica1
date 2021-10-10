c2b8ad4904125b95526afe98889875e46f94f254
Michael-Vinces <e1313198382@live.uleam.edu.ec>
2021-10-10 18:19:55 -0500

    Agregar archivos de proyecto.

---
+++ Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelVinces3A.Practica1
+{
+    class Factorial
+    {
+        private int numeroFact;
+        private int factorialTotal = 1;
+
+        public int NumeroFact { get => numeroFact; set => numeroFact = value; }
+        public int FactorialTotal { get => factorialTotal; set => factorialTotal = value; }
+    }
+}
---
+++ Sumatoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelVinces3A.Practica1
+{
+    class Sumatoria
+    {
+         private int numeroSum;
+         private int sumatoriaTotal;
+
+         public int NumeroSum { get => numeroSum; set => numeroSum = value; }
+         public int SumatoriaTotal { get => sumatoriaTotal; set => sumatoriaTotal = value; }
+    }
+}
